49922c08ac77cee49bb4fec31cf7bb0af03db738
Zakaria Moslim <z.zza@hotmail.com>
2018-10-23 16:39:09 +0200

    add address and contact service

---
+++ services/address-service.js
@@ -0,0 +1,50 @@
+const mongoose = require('mogoose');
+mongoose.connect('mongodb://localhost/contact-mongoose', {usNewUrlParser: true});
+
+const db = mongoose.connetion;
+const address = require('../models/address');
+
+db.on('error', console.error);
+
+class AddressService {
+  static async create(data){
+    const address = new Address(data);
+    return await address.save();
+  }
+
+  static async retrive(id){
+    let data;
+
+    if(id){
+      data = await Address.findById(id)
+      .populate('contacts')
+      .exec();
+      console.log(data.contacts);
+    } else {
+      data = await Address.find().exec();
+    }
+    if(!data){
+      throw new Error('Cannot retrive data')
+    }
+    return data;
+  }
+  static async update(id, data){
+    const team = await Address.findByIdAndUpdate(id, data, {
+      new: true,
+      runValidators: true
+    });
+    if(!contact){
+      throw new Error('Cannot update data');
+    }
+    return address;
+  }
+  static async delete(id){
+    const deleted = await Address.findByIdAndDelete(id);
+    if(!deleted){
+      throw new Error ('Cannot delete data');
+    }
+    return deleted;
+  }
+}
+
+module.exports = AddressService;
---
+++ services/contact-service.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/contact-mongoose', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+const contact = require('../models/contact');
+
+
+db.on('error', console.error);
+
+class ContactServise {
+  static async create(data){
+    const newContact = new Contact(data);
+
+    return await newContact.save();
+  }
+
+  static async retrive({id, addressid}){
+    let data;
+
+    if (id){
+      data = await Contact.findById(id)
+      .populate('address')
+      .exec();
+    } else if(addressid){
+      data = await Contact.find({address: addressid}).exec();
+    } else {
+      data = await Contact.find().exec();
+    }
+
+    if(!data){
+      throw new Error('Cannot retrive data');
+    }
+
+    return data;
+    }
+
+    static async update(id, data){
+      const updated = await Contact.findByIdAndUpdate(id, data, {
+        new: true,
+        runValidators: true
+      });
+
+      if (!updated){
+        throw new Error('Cannot update data');
+      }
+
+      return updated;
+    }
+
+    static async delete(id){
+      const deleted = await Contact.findByIdAndDelete(id);
+
+      if (!deleted){
+        throw new Error('Cannot delete data');
+      }
+
+      return deleted;
+  }
+}
+
+module.exports = ContactServise;
