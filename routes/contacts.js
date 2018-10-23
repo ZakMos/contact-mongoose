06fa954dd29cca9376b78c87a393d5895091d637
Zakaria Moslim <z.zza@hotmail.com>
2018-10-23 15:27:34 +0200

    add contacts router

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,61 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const Boom = require('boom');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const contactService = require('../services/contact-services');
+const router = express.Router();
+
+
+router.post('/', async (req, res, next) => {
+  try{
+    const contact = await contactService.creat(req.body);
+    res.json(contact);
+  }catch(err){
+    if(err.name === 'ValidationError'){
+      next(Boom.badRequest(err));
+    }
+      next(Boom.badImplemation(err));
+  }
+});
+
+router.get('/', async(req, res) => {
+  const contact = await contactService.retrive();
+  res.json(contact);
+});
+
+router.get('/:id'), async(req, res, next) => {
+  const {id} = req.params;
+  try{
+    const contact = await contactService.retrive(id);
+    res.json(contact);
+  } catch(err){
+    next(Boom.notFound(`No such contact with id: ${id}`));
+  }
+});
+
+
+
+router.put('/:id', async(req, res, next)=> {
+  const {id} = req.params;
+  try{
+    const updated = await contactService.update(id);
+    res.json(updated);
+  } catch(err){
+    if(err.name === 'ValidatationError'){
+      next(Boom.badRequest(err));
+    } else{
+      next(Boom.notFound(`No such contact with id: ${id}`));
+    }
+  }
+});
+
+router.delete('/:id', async(req, res, next)=> {
+  const {id} = req.params;
+  try{
+    const deleted = await contactService.delete(id);
+    res.json(deleted);
+  }catch(err){
+    next(Boom.notFound(`No such contact with id ${id}`));
+  }
 });
 
 module.exports = router;
